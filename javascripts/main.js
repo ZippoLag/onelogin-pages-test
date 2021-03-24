48a0b8b6c80caf0db2326c02b1126a9087ca5d66
Sebastián Vansteenkiste <Sebastian.Vansteenkiste@rescue.org>
2021-03-24 15:13:26 -0400

    getting logged-in user from local storage

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,59 +1,79 @@
-document.getElementById('login').addEventListener("click", redirectToLogin, false);
-
 Oidc.Log.logger = console;
 Oidc.Log.level = Oidc.Log.INFO;
 
 //
 // OIDC Client Configuration
 //
-const ONELOGIN_CLIENT_ID = 'b2afa500-5a9e-0139-e6b4-0668c6da8869185719';
-const ONELOGIN_SUBDOMAIN = 'the-irc-dev';
+const ONELOGIN_CLIENT_ID = "b2afa500-5a9e-0139-e6b4-0668c6da8869185719";
+const ONELOGIN_SUBDOMAIN = "the-irc-dev";
 
 var settings = {
-    authority: 'https://' + ONELOGIN_SUBDOMAIN + '.onelogin.com/oidc/2',
-    client_id: ONELOGIN_CLIENT_ID,
-    redirect_uri: window.location.href,
-    response_type: 'id_token token',
-    scope: 'openid profile',
+  authority: "https://" + ONELOGIN_SUBDOMAIN + ".onelogin.com/oidc/2",
+  client_id: ONELOGIN_CLIENT_ID,
+  redirect_uri: window.location.href,
+  response_type: "id_token token",
+  scope: "openid profile",
 
-    filterProtocolClaims: true,
-    loadUserInfo: true
+  filterProtocolClaims: true,
+  loadUserInfo: true,
 };
 var mgr = new Oidc.UserManager(settings);
 
-//
-// Redirect to OneLogin to authenticate the user
-//
-function redirectToLogin(e) {
-  e.preventDefault();
+mgr.getUser().then((user) => {
+  if (user) {
+    document.getElementById("user").innerHTML = user.profile.email;
+  } else {
+    document
+      .getElementById("login")
+      .addEventListener("click", redirectToLogin, false);
 
-  mgr.signinRedirect({state:'some data'}).then(function() {
-      console.log("signinRedirect done");
-  }).catch(function(err) {
-      console.log(err);
-  });
-}
+    //
+    // Redirect to OneLogin to authenticate the user
+    //
+    function redirectToLogin(e) {
+      e.preventDefault();
 
-//
-// Handle the authentication response returned
-// by OneLogin after the user has attempted to authenticate
-//
-function processLoginResponse() {
-  mgr.signinRedirectCallback().then(function(user) {
-      console.log("signed in", user);
+      mgr
+        .signinRedirect({ state: "some data" })
+        .then(function () {
+          console.log("signinRedirect done");
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    }
 
-      document.getElementById("loginResult").innerHTML = '<h3>Success</h3><pre><code>' + JSON.stringify(user, null, 2) + '</code></pre>'
+    //
+    // Handle the authentication response returned
+    // by OneLogin after the user has attempted to authenticate
+    //
+    function processLoginResponse() {
+      mgr
+        .signinRedirectCallback()
+        .then(function (user) {
+          console.log("signed in", user);
 
-  }).catch(function(err) {
-    console.log(err);
-    document.getElementById("error").innerHTML = '<h3>Error</h3><pre><code>' + err + '</code></pre>'
-  });
-}
+          document.getElementById("loginResult").innerHTML =
+            "<h3>Success</h3><pre><code>" +
+            JSON.stringify(user, null, 2) +
+            "</code></pre>";
 
-//
-// Look out for a authentication response
-// then log it and handle it
-//
-if (window.location.href.indexOf("#") >= 0) {
-  processLoginResponse();
-}
+          mgr.storeUser(user);
+          debugger;
+        })
+        .catch(function (err) {
+          console.log(err);
+          document.getElementById("error").innerHTML =
+            "<h3>Error</h3><pre><code>" + err + "</code></pre>";
+        });
+    }
+
+    //
+    // Look out for a authentication response
+    // then log it and handle it
+    //
+    if (window.location.href.indexOf("#") >= 0) {
+      processLoginResponse();
+    }
+  }
+});
